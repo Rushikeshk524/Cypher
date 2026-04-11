713fa5fae68ccfab22f2ccab99bc960c93d6f1a8
Rushikesh <rushikeshk524@gmail.com>
2026-04-11 16:31:11 +0530

    vaildParenthesis

--- rushikesh java projects/Pattern Problem/ValidParenthesis.java
+++ rushikesh java projects/Pattern Problem/ValidParenthesis.java
@@ -1,32 +1,27 @@
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
-//works only for ([]) not for {()[]}
+
 class ValidParenthesis{
     public static boolean isValid(String s)
     {
         Deque<Character> stack = new ArrayDeque<>();
         for(char c : s.toCharArray())
         {
-            if(c == '(' || c == '{' || c == '[')
-            {
+            if(c == '(' || c == '{' || c == '['){
                 stack.push(c);
-            }
-            
-            else if(c == ')' && stack.peek() == '(')
+            }    
+            else if(c == ')')
             {
-                stack.pop();
+                if(stack.isEmpty() || stack.pop() != '(') return false;
             }
-            else if(c == '}' && stack.peek() == '{')
+            else if(c == '}')
             {
-                stack.pop();
+                if(stack.isEmpty() || stack.pop() != '{') return false;
             }
-            else if(c == ']' && stack.peek() == '[')
+            else if(c == ']')
             {
-                stack.pop();
-            }
-            else
-            {
-                return false;
+                if(stack.isEmpty() || stack.pop() != '[') return false;
             }
         }
         return stack.isEmpty();
@@ -34,7 +29,7 @@ class ValidParenthesis{
     }
 
     public static void main(String[] args) {
-        String s = "{()}";
+        String s = "(){{}[]()}{[()]}";
         boolean result = isValid(s);
         System.out.println(result);
     }
